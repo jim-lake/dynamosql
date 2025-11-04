e0c3aec20d3fe8c1a6e79bf6a6ba921af3436c8b
Jim Lake <jim@blueskylabs.com>
2025-11-04 21:11:31 +0000

    types

--- apps/dynamosql/rollup.config.mjs
+++ apps/dynamosql/rollup.config.mjs
@@ -1,19 +1,27 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/dynamosql.js',
-    format: 'cjs',
-    sourcemap: true,
-    sourcemapExcludeSources: (source) => source.includes('src/vendor/'),
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/dynamosql.js',
+      format: 'cjs',
+      sourcemap: true,
+      sourcemapExcludeSources: (source) => source.includes('src/vendor/'),
+    },
+    plugins: [resolve(), commonjs()],
+    external: [
+      '@aws-sdk/client-dynamodb',
+      'async',
+      'big-integer',
+      'sqlstring',
+    ],
   },
-  plugins: [resolve(), commonjs()],
-  external: [
-    '@aws-sdk/client-dynamodb',
-    'async',
-    'big-integer',
-    'sqlstring',
-  ],
-};
+  {
+    input: 'src/index.js',
+    output: { file: 'dist/dynamosql.d.ts', format: 'es' },
+    plugins: [dts()],
+  },
+];
