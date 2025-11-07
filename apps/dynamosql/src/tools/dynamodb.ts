01474a8455f0098ab1cd9eb2157fdb9bb4907652
Jim Lake <jim@blueskylabs.com>
2025-11-07 05:19:58 +0000

    cleanup paged send stuff

--- apps/dynamosql/src/tools/dynamodb.ts
+++ apps/dynamosql/src/tools/dynamodb.ts
@@ -1,5 +1,3 @@
-import asyncForever from 'async/forever';
-import asyncTimesLimit from 'async/timesLimit';
 import {
   DynamoDBClient,
   BatchExecuteStatementCommand,
@@ -151,141 +149,90 @@ export class DynamoDB {
   async updateItems(params: any): Promise<any> {
     const BATCH_LIMIT = 100;
     const { table, key_list, list } = params;
-    const batch_count = Math.ceil(list.length / BATCH_LIMIT);
     const prefix = `UPDATE ${escapeIdentifier(table)} SET `;
-    const err_list: any[] = [];
-    const result_list: any[] = [];
-    await new Promise((resolve, reject) => {
-      asyncTimesLimit(
-        batch_count,
-        QUERY_LIMIT,
-        (batch_index: number, done: any) => {
-          const sql_list: string[] = [];
-          const start = BATCH_LIMIT * batch_index;
-          const end = Math.min(start + BATCH_LIMIT, list.length);
-          for (let i = start; i < end; i++) {
-            const item = list[i];
-            const sets = item.set_list
-              .map((object: any) => {
-                const { column, value } = object;
-                return `${escapeIdentifier(column)} = ${escapeValue(value)}`;
+    return parallelBatch(
+      list,
+      BATCH_LIMIT,
+      QUERY_LIMIT,
+      async (batch: any[]) => {
+        const sql_list: string[] = [];
+        for (const item of batch) {
+          const sets = item.set_list
+            .map((object: any) => {
+              const { column, value } = object;
+              return `${escapeIdentifier(column)} = ${escapeValue(value)}`;
+            })
+            .join(', ');
+          const cond =
+            ' WHERE ' +
+            key_list
+              .map((key: string, j: number) => {
+                const value = item.key[j];
+                return `${escapeIdentifier(key)} = ${convertValueToPQL(value)}`;
               })
-              .join(', ');
-            const cond =
-              ' WHERE ' +
-              key_list
-                .map((key: string, j: number) => {
-                  const value = item.key[j];
-                  return `${escapeIdentifier(key)} = ${convertValueToPQL(value)}`;
-                })
-                .join(' AND ');
-            sql_list.push(prefix + sets + cond);
-          }
-          this.transactionQL(sql_list).then(
-            (result) => {
-              for (let i = start; i < end; i++) {
-                result_list[i] = result?.[i - start];
-              }
-              done();
-            },
-            (err) => {
-              for (let i = start; i < end; i++) {
-                err_list[i] = err?.[i - start];
-              }
-              done(err);
-            }
-          );
-        },
-        (err: any) => (err ? reject(err_list) : resolve(result_list))
-      );
-    });
-    return result_list;
+              .join(' AND ');
+          sql_list.push(prefix + sets + cond);
+        }
+        return this.transactionQL(sql_list);
+      }
+    );
   }
 
   async putItems(params: any): Promise<void> {
     const BATCH_LIMIT = 100;
     const { table, list } = params;
-    const batch_count = Math.ceil(list.length / BATCH_LIMIT);
     const err_list: any[] = [];
-    await new Promise((resolve, reject) => {
-      asyncTimesLimit(
-        batch_count,
-        QUERY_LIMIT,
-        (batch_index: number, done: any) => {
-          const start = BATCH_LIMIT * batch_index;
-          const end = start + BATCH_LIMIT;
-          const item_list = list.slice(start, end);
-          const input = {
-            TransactItems: item_list.map((item: any) => ({
-              Put: {
-                TableName: table,
-                Item: nativeToValue(item).M,
-              },
-            })),
-          };
-          const command = new TransactWriteItemsCommand(input);
-          this.client.send(command).then(
-            () => done(),
-            (err: any) => {
-              if (
-                err?.name === 'TransactionCanceledException' &&
-                err.CancellationReasons?.length > 0
-              ) {
-                err.CancellationReasons.forEach(
-                  (cancel_err: any, i: number) => {
-                    err_list[start + i] = {
-                      err: convertError(cancel_err),
-                      parent: cancel_err,
-                    };
-                  }
-                );
-              } else {
-                err_list[start] = {
-                  err: convertError(err),
-                  parent: err,
-                };
-              }
-              done(err || 'unknown');
-            }
-          );
-        },
-        (err: any) => (err ? reject(err_list) : resolve(undefined))
-      );
-    });
+    try {
+      await parallelBatch(list, BATCH_LIMIT, QUERY_LIMIT, async (batch, i) => {
+        const input = {
+          TransactItems: batch.map((item: any) => ({
+            Put: {
+              TableName: table,
+              Item: nativeToValue(item).M,
+            },
+          })),
+        };
+        const command = new TransactWriteItemsCommand(input);
+        try {
+          await this.client.send(command);
+          return batch.map(() => undefined);
+        } catch (err: any) {
+          const start = i * BATCH_LIMIT;
+          if (
+            err?.name === 'TransactionCanceledException' &&
+            err.CancellationReasons?.length > 0
+          ) {
+            err.CancellationReasons.forEach((cancel_err: any, j: number) => {
+              err_list[start + j] = {
+                err: convertError(cancel_err),
+                parent: cancel_err,
+              };
+            });
+          } else {
+            err_list[start] = {
+              err: convertError(err),
+              parent: err,
+            };
+          }
+          throw err;
+        }
+      });
+    } catch {
+      throw err_list;
+    }
   }
 
   async getTableList(): Promise<string[]> {
     const command = new ListTablesCommand({ Limit: 100 });
-    let next_token: any;
     const results: string[] = [];
-    await new Promise((resolve, reject) => {
-      asyncForever(
-        (done: any) => {
-          if (next_token) {
-            command.input.ExclusiveStartTableName = next_token;
-          }
-          this.client.send(command).then(
-            (result: any) => {
-              let err: any;
-              result.TableNames.forEach((table: string) => results.push(table));
-              next_token = result?.LastEvaluatedTableName;
-              if (!next_token) {
-                err = 'stop';
-              }
-              done(err);
-            },
-            (err) => done(err)
-          );
-        },
-        (err: any) => {
-          if (err === 'stop') {
-            resolve(results);
-          } else {
-            reject(err);
-          }
-        }
-      );
-    });
+    while (true) {
+      const result = await this.client.send(command);
+      result.TableNames?.forEach((table: string) => results.push(table));
+      if (!result.LastEvaluatedTableName) {
+        break;
+      }
+      command.input.ExclusiveStartTableName = result.LastEvaluatedTableName;
+    }
     return results;
   }
 
@@ -394,45 +341,25 @@ export class DynamoDB {
   }
 
   private async _pagedSend(command: any): Promise<any> {
-    let next_token: any;
     const results: any[] = [];
-    await new Promise((resolve, reject) => {
-      asyncForever(
-        (done: any) => {
-          if (next_token) {
-            command.input.NextToken = next_token;
-          }
-          this.client.send(command).then(
-            (result: any) => {
-              let err;
-              const list: any[] = convertSuccess(result)[1];
-              list?.forEach?.((item: any) => {
-                results.push(item);
-              });
-
-              next_token = result?.NextToken;
-              if (!err && !next_token) {
-                err = 'stop';
-              }
-              done(err);
-            },
-            (err: any) => {
-              if (err.Item) {
-                results.push(err.Item);
-              }
-              done(convertError(err));
-            }
-          );
-        },
-        (err: any) => {
-          if (err === 'stop') {
-            resolve(results);
-          } else {
-            reject(err);
-          }
+    while (true) {
+      try {
+        const result: any = await this.client.send(command);
+        const list: any[] = convertSuccess(result)[1];
+        list?.forEach?.((item: any) => {
+          results.push(item);
+        });
+        if (!result?.NextToken) {
+          break;
         }
-      );
-    });
+        command.input.NextToken = result.NextToken;
+      } catch (err: any) {
+        if (err.Item) {
+          results.push(err.Item);
+        }
+        throw convertError(err);
+      }
+    }
     return results;
   }
 }
