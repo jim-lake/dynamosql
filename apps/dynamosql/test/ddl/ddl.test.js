d1403f4de28fc549c420c5b96085960c0b12f498
jlake <jim@blueskylabs.com>
2025-11-25 10:00:18 -0800

    longer ddl timeout

--- apps/dynamosql/test/ddl/ddl.test.js
+++ apps/dynamosql/test/ddl/ddl.test.js
@@ -4,50 +4,50 @@ const path = require('node:path');
 describe('DDL', function () {
   runTests('transactions.sql', path.join(__dirname, 'transactions.sql'), {
     skipAffected: true,
-    timeout: 15000,
+    timeout: 30000,
   });
 
   runTests(
     'create.sql',
     path.join(__dirname, 'create.sql'),
-    { skipAffected: true, timeout: 15000 },
+    { skipAffected: true, timeout: 30000 },
     true
   );
 
   runTests(
     'ctas.sql',
     path.join(__dirname, 'ctas.sql'),
-    { skipAffected: true, timeout: 15000 },
+    { skipAffected: true, timeout: 30000 },
     true
   );
   runTests(
     'alter.sql',
     path.join(__dirname, 'alter.sql'),
-    { skipAffected: true, timeout: 15000 },
+    { skipAffected: true, timeout: 30000 },
     true
   );
   runTests(
     'create_variations.sql',
     path.join(__dirname, 'create_variations.sql'),
-    { skipAffected: true, timeout: 15000 },
+    { skipAffected: true, timeout: 30000 },
     true
   );
   runTests(
     'drop.sql',
     path.join(__dirname, 'drop.sql'),
-    { skipAffected: true, timeout: 15000 },
+    { skipAffected: true, timeout: 30000 },
     true
   );
   runTests(
     'show.sql',
     path.join(__dirname, 'show.sql'),
-    { skipAffected: true, timeout: 15000 },
+    { skipAffected: true, timeout: 30000 },
     true
   );
   runTests(
     'error_cases.sql',
     path.join(__dirname, 'error_cases.sql'),
-    { skipAffected: true, timeout: 15000 },
+    { skipAffected: true, timeout: 30000 },
     true
   );
 });
