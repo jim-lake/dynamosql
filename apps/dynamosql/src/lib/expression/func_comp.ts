c117907fb78985ba02271d982b49a0000163d81b
jlake <jim@blueskylabs.com>
2025-12-03 20:31:39 -0800

    convert better

--- apps/dynamosql/src/lib/expression/func_comp.ts
+++ apps/dynamosql/src/lib/expression/func_comp.ts
@@ -14,121 +14,7 @@ export function greatest(
   expr: Function,
   state: EvaluationState
 ): EvaluationResult {
-  const { timeZone } = state.session;
-  let value: EvaluationResult['value'];
-  const names: string[] = [];
-
-  const values = expr.args?.value?.map((sub) => getValue(sub, state)) ?? [];
-  if (values.length < 2) {
-    return {
-      err: { err: 'ER_WRONG_PARAMCOUNT_TO_NATIVE_FCT', args: ['GREATEST'] },
-      value: null,
-      type: 'null',
-    };
-  }
-
-  for (const sub of values) {
-    names.push(sub.name ?? '');
-    if (sub.err) {
-      return sub;
-    }
-    if (sub.value === null) {
-      value = null;
-    }
-  }
-  const { compare_type, type } = _unionType(values);
-
-  let decimals = 0;
-  let convert_func:
-    | typeof convertDateTime
-    | typeof convertDate
-    | typeof convertTime
-    | undefined = undefined;
-  if (value !== null) {
-    switch (compare_type) {
-      case 'datetime':
-        convert_func = convertDateTime;
-      // eslint-disable-next-line no-fallthrough
-      case 'date':
-        convert_func ??= convertDate;
-      // eslint-disable-next-line no-fallthrough
-      case 'time':
-        convert_func ??= convertTime;
-        {
-          const ret = _gtList(
-            values,
-            (val) => convert_func?.({ value: val, timeZone }) ?? null
-          );
-          if (ret === null) {
-            value = null;
-          } else {
-            decimals = ret.decimals;
-            if (
-              ret.value &&
-              type === 'string' &&
-              typeof ret.value !== 'string'
-            ) {
-              value = ret.value.toString({ timeZone, decimals });
-            } else {
-              value = ret.value;
-            }
-          }
-        }
-        break;
-      case 'longlong':
-        {
-          let val: bigint | number | undefined = undefined;
-          for (const sub of values) {
-            if (typeof val === 'bigint' || typeof sub.value === 'bigint') {
-              const val_big = BigInt(sub.value as number);
-              if (val === undefined || val_big > val) {
-                val = val_big;
-              }
-            } else {
-              const val_num = Number(sub.value);
-              if (val === undefined || val_num > val) {
-                val = val_num;
-              }
-            }
-          }
-          value = val;
-        }
-        break;
-      case 'double':
-      case 'number':
-        {
-          let val: number | undefined = undefined;
-          for (const sub of values) {
-            const val_n = Number(sub.value);
-            if (val === undefined || val_n > val) {
-              val = val_n;
-            }
-          }
-          value = val;
-        }
-        break;
-      default:
-      case 'string':
-        {
-          let val: string | undefined = undefined;
-          for (const sub of values) {
-            const val_s = String(sub.value);
-            if (val === undefined || val_s > val) {
-              val = val_s;
-            }
-          }
-          value = val;
-        }
-        break;
-    }
-  }
-  return {
-    err: null,
-    value,
-    type,
-    name: `GREATEST(${names.join(', ')})`,
-    decimals,
-  };
+  return _compare(expr, state, 'GREATEST', _gtList, (a, b) => a > b);
 }
 export function least(
   expr: Function,
@@ -144,9 +30,10 @@ type ConvertFunction<T extends Comparable<T>> = (
 ) => T | null;
 function _gtList<T extends Comparable<T>>(
   list: EvaluationResult[],
+  timeZone: string,
   convert: ConvertFunction<T>
 ): { value: T | string | null; decimals: number } | null {
-  const convert_list = list.map(item => convert(item.value));
+  const convert_list = list.map((item) => convert(item.value));
   const decimals = list.reduce((memo, r, i) => {
     const converted = convert_list[i];
     const dec = converted?.getDecimals
@@ -322,8 +209,7 @@ export function _compare(
             values,
             timeZone,
             (val, dec) =>
-              convert_func?.({ value: val, timeZone, decimals: dec }) ??
-              null
+              convert_func?.({ value: val, timeZone, decimals: dec }) ?? null
           );
           if (ret === null) {
             value = null;
