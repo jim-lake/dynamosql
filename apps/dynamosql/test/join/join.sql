c4a4879f81fc1b56a089a024e435a756eb910a8b
jlake <jim@blueskylabs.com>
2025-12-10 09:33:47 -0800

    self join

--- apps/dynamosql/test/join/join.sql
+++ apps/dynamosql/test/join/join.sql
@@ -15,3 +15,7 @@ SELECT foo.*, bar.* FROM _dynamodb.foo, _dynamodb.bar ORDER BY foo.id ASC, bar.i
 SELECT foo.* FROM _dynamodb.foo, _dynamodb.bar ORDER BY foo.id ASC, bar.id ASC;
 SELECT bar.* FROM _dynamodb.foo, _dynamodb.bar ORDER BY foo.id ASC, bar.id ASC;
 
+SELECT s1.id AS s1id,  s2.id as s2id, s1.other AS s1other, s2.other AS s2other
+  FROM _dynamodb.bar s1
+  LEFT JOIN _dynamodb.bar s2 ON s2.otherother = s1.id
+  ORDER BY s1id ASC;
